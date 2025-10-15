b62f84db2fd8abef0f2c07fe6a89fe010cd89c71
jtrajano <jlt212000@yahoo.com>
2025-10-15 15:00:42 +0800

    Downgrade to .net8

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -1,19 +1,21 @@
 using SimpleAPI.Extensions;
-
 namespace SimpleAPI;
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder();
         // Add services to the container.
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
         var app = builder.Build();
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+
         }
         app.UseHttpsRedirection();
+        app.UseSwagger();
+        app.UseSwaggerUI();
         app.MapEndpoints();
         app.Run();
     }
